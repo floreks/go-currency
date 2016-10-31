6804a0b00cb8deb5ffbb807f9d16f5cf8cc4e0c5
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2016-10-31 16:10:58 +0100

    Add license information

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # go-currency
 
+[![Build Status](https://travis-ci.org/floreks/go-currency.svg?branch=master)](https://travis-ci.org/floreks/go-currency) [![Go Report Card](https://goreportcard.com/badge/github.com/floreks/go-currency)](https://goreportcard.com/report/github.com/floreks/go-currency)
+
 go-currency is a currency exchange rate converter service written in Go.
--- service/converter/converter.go
+++ service/converter/converter.go
@@ -1,3 +1,15 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package converter
 
 import (
@@ -79,7 +91,7 @@ func (c ConverterService) parseConverterParameters(
 
 	amountParam := request.QueryParameter("amount")
 	amount, err := strconv.ParseFloat(amountParam, 64)
-	if err != nil {
+	if err != nil || amount < 0.0 {
 		log.Printf("Provided amount is invalid or empty: '%s'.", amountParam)
 		return nil, errors.New(fmt.Sprintf("Provided amount is invalid or empty: '%s'.", amountParam))
 	}
