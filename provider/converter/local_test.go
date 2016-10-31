98755f397c77ceb820ff8cf7b93b1e05d8ae93e3
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2016-10-31 18:18:31 +0100

    More tests

--- provider/converter/local_test.go
+++ provider/converter/local_test.go
@@ -13,6 +13,7 @@
 package converter
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 )
@@ -45,6 +46,51 @@ func TestConvertLocal(t *testing.T) {
 		if !reflect.DeepEqual(actual, c.expected) {
 			t.Errorf("LocalProvider.convert(%v, %v) == \ngot: %v, \nexpected %v",
 				c.rates, c.amount, actual, c.expected)
+		}
+	}
+}
+
+func TestConvert(t *testing.T) {
+	provider := new(LocalProvider)
+	cases := []struct {
+		currency      string
+		amount        float64
+		expected      *ConverterResponse
+		expectedError error
+	}{
+		{
+			CURRENCY_PLN, 10,
+			&ConverterResponse{
+				Amount:   10,
+				Currency: CURRENCY_PLN,
+				Converted: ConvertedRates{
+					"IDR": 32982, "JPY": 265.65, "ZAR": 34.31, "BRL": 8.05, "HRK": 17.35,
+					"MXN": 47.87, "MYR": 10.62, "NOK": 20.88, "USD": 2.53, "CNY": 17.14,
+					"HKD": 19.61, "PHP": 122.61, "RUB": 160.01, "CHF": 2.5, "NZD": 3.54,
+					"SEK": 22.79, "EUR": 2.31, "ILS": 9.74, "GBP": 2.08, "KRW": 2899.6,
+					"BGN": 4.52, "CAD": 3.39, "CZK": 62.44, "DKK": 17.19, "HUF": 712.69,
+					"INR": 168.89, "AUD": 3.33, "TRY": 7.85, "SGD": 3.52, "THB": 88.56,
+					"RON": 10.41,
+				}}, nil,
+		},
+		{
+			"ERR_CURRENCY", 10,
+			nil,
+			errors.New("Currency ERR_CURRENCY not supported by local provider."),
+		},
+	}
+
+	for _, c := range cases {
+		actual, err := provider.Convert(c.amount, c.currency)
+
+		if !reflect.DeepEqual(err, c.expectedError) {
+			t.Errorf("LocalProvider.Convert(%f, %s) == \ngot: %s, \nexpected: %s",
+				c.amount, c.currency, err, c.expectedError)
+		}
+
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("LocalProvider.convert(%f, %s) == \ngot: %v, \nexpected: %v",
+				c.amount, c.currency, actual, c.expected)
 		}
 	}
 }
